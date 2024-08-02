ff9fdabfb11b25775d502fdf474c58bd3a241744
taisVioleta <gutierrezgonzaleztaisvioleta@gmail.com>
2024-08-02 08:57:16 -0600

    Add files via upload
    Ejercicios JS 04

---
+++ EjerciciosArray.js
@@ -0,0 +1,56 @@
+//*E J E R C I C I O    1  Sum of Resistors in Series 
+let r1 = [-1, 5 , 6 , 3];
+let plus = 0;
+for (let i = 0; i < r1.length; i++) {
+    plus += r1[i];
+}
+console.log(`The series resistive sum of this circuit is: ${plus} ohms`);   
+
+
+let r2 = [14, 3.5, 6,];
+let add = 0;
+for (let i = 0; i < r2.length; i++) {
+    add += r2[i];
+}
+console.log(`The series resistive sum of this circuit is: ${add} ohms`);  
+
+
+let r3 = [8, 15, 100,];
+let suma = 0;
+for (let i = 0; i < r3.length; i++) {
+    suma += r3[i];
+}
+console.log (`The series resistive sum of this circuit is: ${suma}  ohms`);  
+
+
+
+//*E J E R C I C I O   2   Number divided into halves
+i=2
+console.log(Array.from('ii'));
+
+
+//*E J E R C I C I O   3   Secret Society
+
+
+
+
+//*E J E R C I C I O   4   Online status
+
+const users = ["Violeta", "Damaris", "Mara", "Zai", "Mitzi", "Diana", "Astrid", "Karla"];
+const numberOfUsers = users.length;
+
+console.log ("Bienvenido al canal de comunicación Stella")
+console.log (users.slice(0, 3));
+let others= numberOfUsers - 3;
+console.log(" y otros " + others);
+
+
+
+//*E J E R C I C I O   5   Array of Multiples
+
+
+//*E J E R C I C I O   6   Positive dominance in Array
+
+
+
+//*E J E R C I C I O   7   Antipodal Average
